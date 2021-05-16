4243b7319fd8d13e340a1a61a89bb28942cd5a7b
Ming07 <q502489978@live.com>
2021-05-16 23:56:44 +0800

    refactor: 整理插件逻辑

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
 import path from "path";
 import { Plugin } from "vite";
 import ws from "ws";
+import { listenExitProcess } from "./process";
 import { Options, ResolvedOptions } from "./type";
 import {
   findBackgroundEntry,
-  genInputConfig,
+  genBackgroundHtml,
+  genBackgroundInput,
+  genContentInput,
   genManifest,
   genReloadHtml,
   isBackgroundOpts,
 } from "./utils";
 import { copyDir } from "./utils";
+import { createWebSocketServer } from "./ws";
 export { reload } from "./reload";
 
 export default function crxPlugin(
@@ -31,74 +35,58 @@ export default function crxPlugin(
     "node_modules/vite-plugin-crx/client"
   );
   const reloadCode =
-    `import { reload } from "vite-plugin-crx"\n` +
-    `reload(${options.host},${options.port})`;
+    `import { reload } from "./reload.js"\n` +
+    `reload("${options.host}",${options.port})`;
   // 创建一个ws服务
-  let wss = new ws.Server({
-    host: options.host,
-    port: options.port,
-  });
-  // 扩展客户端
-  let client: ws;
-  wss.on("connection", function connection(ws) {
-    ws.on("message", function incoming(message) {
-      console.log("received: %s", message);
-    });
-    ws.send("something");
-    client = ws;
-  });
+  const ws = createWebSocketServer(options)
+  // 监听进程退出
+  listenExitProcess(ws)
   return {
     name: "vite:chrome-extension",
     config(config) {
       const { mode } = config;
-      const { background } = options;
+      const { background,content } = options;
 
       outDir = outDir + "/" + (config.build?.outDir || "dist");
       // 开发模式下，需要热重载
       reloadEntry = mode === "development" ? reloadEntry : "";
 
-      // 开发模式下，若background不存在入口，则生成一个html作为扩展入口来插入热重载代码
-      let isGenHtml = false;
-      if (mode === "development" && !isBackgroundOpts(background)) {
-        genReloadHtml(clientDir, options);
-        isGenHtml = true;
-      }
-
       // 生成清单文件manifest.json
-      genManifest(clientDir, options, isGenHtml);
+      genManifest(clientDir, options);
 
       // 根据配置的background和content生成vite的入口配置
-      const inputOptions = genInputConfig(options);
+      const backgroundInput = genBackgroundInput(background)
+      const contentInput = genContentInput(content);
+      // 根据background入口配置生成background的html，js全部以es module引入
+      genBackgroundHtml(clientDir,backgroundInput, mode === "development")
       return {
         build: {
           rollupOptions: {
-            external: "vite-plugin-crx",
-            input: inputOptions,
+            external: "./reload.js",
+            input: {...backgroundInput,...contentInput},
             output: {
               entryFileNames: "[name].js",
               chunkFileNames: "[name].js",
               assetFileNames: "[name].js",
+              format: "esm"
             },
           },
         },
       };
     },
-
     transform(code, id) {
-      // TODO：找到background入口中第一个，插入热重载代码
-      if (id.endsWith(reloadEntry)) {
+      // TODO：找到background入口中第一个，插入热重载代码      
+      if (reloadEntry && id.endsWith(reloadEntry)) {
         code += `${reloadCode}`;
       }
       return code;
     },
     buildEnd() {
-      console.log(`\n build end`);
       // 复制client目录下的代码到构建目录
       copyDir(clientDir, outDir);
-
-      if (client) {
-        client.send("reload");
-      }
+      setTimeout(() => {
+        ws.send({type: "update"})
+      },500)
     },
   };
 }
