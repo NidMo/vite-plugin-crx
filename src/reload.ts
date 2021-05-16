9e5703c29d4dad26c92648854829179a60505a1b
Ming07 <q502489978@live.com>
2021-05-16 23:56:02 +0800

    refactor: [reload] 修改热重载逻辑

--- src/reload.ts
+++ src/reload.ts
@@ -1,15 +1,23 @@
+import { HMRPayload } from "./ws";
+
 export const reload = function (host: string, port: string) {
   // Create WebSocket connection.
   const socket = new WebSocket(`ws://${host}:${port}`);
   // Connection opened
-  socket.addEventListener("connection", function (event) {
-    socket.send("Hello Server!");
-  });
-
-  // Listen for messages
-  socket.addEventListener("message", function (event) {
-    if (event.data === "reload") {
-      chrome.runtime.reload();
-    }
+  socket.addEventListener("message", async ({ data }) => {
+    handleMessage(JSON.parse(data));
   });
 };
+
+async function handleMessage(payload: HMRPayload) {
+  switch (payload.type) {
+    case "connected":
+      console.log(`[vite-pluign-crx] connected.`);
+      break;
+    case "update":
+      chrome.runtime.reload();
+      break;
+    default:
+      break;
+  }
+}
